cf438448ea6561744515ee8b9390f097ecbe035a
bob240 <74813341+bob240@users.noreply.github.com>
2025-05-12 17:56:01 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+git clone https://git.eden-emu.dev/eden-emu/eden
+cd eden
+git submodule update --init --recursive
+cd src/android
+chmod +x ./gradlew
+./gradlew assembleRelease --console=plain --info -Dorg.gradle.caching=true
+APK_PATH=$(find app/build/outputs/apk -type f -name "*.apk" | head -n 1)
+mv "$APK_PATH" ./eden.apk
