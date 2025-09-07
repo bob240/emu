9c3c48586d8e7250f9359e6c4fd7490902eaafd6
bob240 <74813341+bob240@users.noreply.github.com>
2025-09-07 18:33:20 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ git submodule update --init --recursive
 cd src/android
 chmod +x ./gradlew
 ./gradlew assembleRelease --console=plain --info -Dorg.gradle.caching=true
+./gradlew bundleRelease
 APK_PATH=$(find app/build/outputs/apk -type f -name "*.apk" | head -n 1)
 AAB_PATH=$(find app/build/outputs/bundle -type f -name ".aab" | head -n 1)
 mv "$APK_PATH" ./eden.apk
