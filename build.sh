05f1453258d656b23c64eb8ab4cecbb6a994831e
bob240 <74813341+bob240@users.noreply.github.com>
2025-09-07 17:56:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ cd src/android
 chmod +x ./gradlew
 ./gradlew assembleRelease --console=plain --info -Dorg.gradle.caching=true
 APK_PATH=$(find app/build/outputs/apk -type f -name "*.apk" | head -n 1)
+AAB_PATH=$(find app/build/outputs/bundle -type f -name ".aab" | head -n 1)
 mv "$APK_PATH" ./eden.apk
+mv "$AAB_PATH" ./eden.aab
 
