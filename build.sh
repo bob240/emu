3c38f7ae7197ebc724cfdf31a05fdde6c8355859
bob240 <74813341+bob240@users.noreply.github.com>
2026-06-10 20:16:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # git clone https://git.eden-emu.dev/eden-emu/eden
 # cd eden
-# git submodule update --init --recursive
+git submodule update --init --recursive
 # git apply ../1.patch
 git clone https://github.com/moze30/winlator-glibc.git
 cd winlator-glibc
